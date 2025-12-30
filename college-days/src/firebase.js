4175ee25218a47230e38d16b19a1702c43709608
Satya <chintukotti78@gmail.com>
2025-12-30 15:11:15 +0530

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details from firebase.js.

--- college-days/src/firebase.js
+++ college-days/src/firebase.js
@@ -2,15 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDSDsAvZ5efAs7qob-g0EevjHIil_EALCw",
-  authDomain: "college-days-c7719.firebaseapp.com",
-  projectId: "college-days-c7719",
-  storageBucket: "college-days-c7719.firebasestorage.app",
-  messagingSenderId: "574464965829",
-  appId: "1:574464965829:web:ecf111260ba54e6e24c4ac",
-  measurementId: "G-9EDNHHJBHK"
-};
+// Firebase Configuration code here
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
